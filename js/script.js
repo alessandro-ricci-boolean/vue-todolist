adba0c5bb99a812e5db5efe50f3ea1480bed4185
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-12-14 00:53:21 +0100

    rough version submitted

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            logo: "./img/logo.png",
+            todo_list: [
+                {
+                    text: "Fare la spesa",
+                    done: true,
+                },
+                {
+                    text: "Fare i compiti",
+                    done: false,
+                },
+                {
+                    text: "Portare a spasso il cane",
+                    done: true,
+                },
+                {
+                    text: "Dare da mangiare ai polli",
+                    done: false,
+                },
+                {
+                    text: "Cucinare la carbonara",
+                    done: true,
+                },
+                {
+                    text: "Pulire casa",
+                    done: false,
+                }
+            ]
+        }
+    },
+    methods:{
+        toggleDone(index){
+            this.todo_list[index].done = !todo_list[index].done;
+        }
+    }
+}).mount("#app");
